ad94d4942b03782d8c91dc6bee85bdd1fb44d868
shariarsultanfahim <shariarsfahim@gmail.com>
2024-03-07 00:30:31 +0600

    Now learboard have top 3 positions card and it also shows their picture

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
 const studentApi = async () => {
-  document.getElementById('loading').classList.remove('hidden');
-  document.getElementById('loading').classList.add('flex');
+  document.getElementById("loading").classList.remove("hidden");
+  document.getElementById("loading").classList.add("flex");
   const res = await fetch("https://sheetdb.io/api/v1/xzodpywpk6f9y");
   const data = await res.json();
   const studentRanking = data;
   showStudentRanking(studentRanking);
 };
 let rank = 1;
-const showStudentRanking = (students) =>{
-    students.sort((a, b) =>{
-      if (a.totalPoint !== b.totalPoint) {
-        return b.totalPoint - a.totalPoint; // Sort by ranking
+const showStudentRanking = (students) => {
+  students.sort((a, b) => {
+    if (a.totalPoint !== b.totalPoint) {
+      return b.totalPoint - a.totalPoint; // Sort by ranking
     } else {
-        return a.solveTime - b.solveTime; // If ranking is the same, sort by total time
+      return a.solveTime - b.solveTime; // If ranking is the same, sort by total time
     }
-    });
-    
-    let rank = 1;
-    students.forEach(student => {
-        const studentTable = document.getElementById('student-rank');
-        const newStudentData = document.createElement('tr');
-        newStudentData.classList = 'text-center bg-transparent hover:bg-stone-200  hover:bg-opacity-50 shadow-lg';
-       if(rank ==1){
-        newStudentData.classList ='text-center bg-green-400 hover:bg-stone-200  hover:bg-opacity-50 shadow-lg';
-       }
-       else if(rank ==2){
-        newStudentData.classList ='text-center bg-green-300 hover:bg-stone-200  hover:bg-opacity-50 shadow-lg';
-       }
-       else if(rank ==3){
-        newStudentData.classList ='text-center bg-green-200 hover:bg-stone-200  hover:bg-opacity-50 shadow-lg';
-       }
-        newStudentData.innerHTML = `
+  });
+
+  let rank = 1;
+  students.forEach((student) => {
+    const studentTable = document.getElementById("student-rank");
+    const newStudentData = document.createElement("tr");
+    newStudentData.classList =
+      "text-center bg-transparent hover:bg-stone-200  hover:bg-opacity-50 shadow-lg";
+
+    if (rank == 1) {
+      document.getElementById("rank-one-img").innerHTML = `
+        <img src="${student.image}" />
+        `;
+      document.getElementById("rank-one-name").innerText = `
+        ${student.name}
+        `;
+      document.getElementById("rank-one-score").innerText = `
+        Score : ${student.totalPoint}
+        `;
+        document.getElementById('rank-one').innerText = `
+        Rank : #${rank++}
+        `;
+    } else if (rank == 2) {
+      document.getElementById("rank-two-img").innerHTML = `
+        <img src="${student.image}" />
+        `;
+      document.getElementById("rank-two-name").innerText = `
+        ${student.name}
+        `;
+      document.getElementById("rank-two-score").innerText = `
+      Score : ${student.totalPoint}
+        `;
+        document.getElementById('rank-two').innerText = `
+        Rank : #${rank++}
+        `;
+    } else if (rank == 3) {
+      document.getElementById("rank-three-img").innerHTML = `
+        <img src="${student.image}" />
+        `;
+      document.getElementById("rank-three-name").innerText = `
+        ${student.name}
+        `;
+      document.getElementById("rank-three-score").innerText = `
+      Score : ${student.totalPoint}
+        `;
+        document.getElementById('rank-three').innerText = `
+        Rank : #${rank++}
+        `;
+    } else {
+      newStudentData.innerHTML = `
         <td>${rank++}</td>
         <td>${student.totalPoint}</td>
         <td>${student.name}</td>
@@ -40,9 +73,10 @@ const showStudentRanking = (students) =>{
         <td>${student.solveCount}</td>
         <td>${student.solveTime}</td>
         `;
-        document.getElementById('loading').classList.add('hidden');
-        document.getElementById('loading').classList.remove('flex');
-        studentTable.append(newStudentData);
-    });
-}
+      document.getElementById("loading").classList.add("hidden");
+      document.getElementById("loading").classList.remove("flex");
+      studentTable.append(newStudentData);
+    }
+  });
+};
 studentApi();
